cfe696d70a893f0e3ba6731ca723c3339cad7025
Derek Adair <d@derekadair.com>
2020-03-24 15:37:23 +0000

    Cleaning up

--- api/config/urls.py
+++ api/config/urls.py
@@ -20,9 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_base.urls')),
 ]
-# static files for debug, also django_debug_toolbar crap
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
